f7d60282fcafd2f0ac413fce80afd9575861f8e4
olo <snash@tutanota.com>
2022-01-11 10:44:04 -0300

    Products comp. on front completed

--- src/components/Product/Styles.js
+++ src/components/Product/Styles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components'
 
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,.2);
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all .3s ease;
+`
+
 const Container = styled.div`
   flex:1;
   margin: 5px;
@@ -10,6 +26,9 @@ const Container = styled.div`
   align-items: center;
   background-color: #f5fbfd;
   position: relative;
+  &:hover ${Info} {
+    opacity: 1; 
+  }
 `
 
 const Circle = styled.div`
@@ -23,21 +42,22 @@ const Image = styled.img`
   height: 75%;
   z-index: 2;
 `
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: gray;
-  z-index: 3;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
 const Icon = styled.div`
-  padding:20px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+  cursor: pointer;
+  transition: all .3s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+
 `
 
 export { Container, Circle, Image, Info, Icon }
