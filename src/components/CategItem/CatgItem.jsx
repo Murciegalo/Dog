670499ffbb354a118e0062e23c7f3df2c86410e8
olo <snash@tutanota.com>
2022-01-11 09:48:42 -0300

    categories comp. completed on front completed

--- src/components/CategItem/CatgItem.jsx
+++ src/components/CategItem/CatgItem.jsx
@@ -1,9 +1,19 @@
-import {Container} from './Styles.js'
+import {
+  Container,
+  Image,
+  Info,
+  Title,
+  Button
+} from './Styles.js'
 
 const CatgItem = ({el}) => {
   return (
     <Container>
-      
+      <Image src={el.img} />
+      <Info>
+        <Title>{el.title}</Title>
+        <Button>Shop Now</Button>
+      </Info>
     </Container>
   )
 }
--- src/components/CategItem/Styles.js
+++ src/components/CategItem/Styles.js
@@ -1,11 +1,40 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
+  flex:1;
+  margin: 3px;
+  height: 70vh;
   position: relative;
-  overflow: hidden;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Info = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+
+`
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 900;
 `
 
-export { Container }
+export { Container, Image, Info, Title, Button }
