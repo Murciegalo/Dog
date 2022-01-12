b667d96ffc60c87611d631d252b62e2984d92fa9
olo <snash@tutanota.com>
2022-01-12 08:42:10 -0300

    footer in progress

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { 
+  Container,
+  Left, 
+  Logo, 
+  Desc, 
+  SocialContainer, 
+  Center, 
+  Right 
+} from '../NewsLetter/NewsLetter.styles'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>LAMA.</Logo>
+        <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quod, assumenda cumque odio aliquam hic vitae asperiores vero neque adipisci illo rerum veritatis aspernatur, fuga qui ipsa. Aut, labore aspernatur?
+          Atque, totam. Vitae excepturi aliquam porro officia quos quia optio at beatae numquam nobis voluptate ipsam natus consequatur incidunt culpa, blanditiis sapiente maiores facere nisi magnam delectus reprehenderit? Incidunt, sapiente?
+          Quibusdam soluta ut eum corporis velit nesciunt maxime reprehenderit? Commodi voluptatum quisquam sunt amet quod quos iure molestiae aperiam odio autem doloribus labore, accusamus pariatur laborum, dolores qui modi nobis?
+        </Desc>
+        <SocialContainer></SocialContainer>
+      </Left>    
+      <Center>
+
+      </Center>
+      <Right>
+        
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
