d4834e13b2d6676fe60289bf6fe3f184683ebe0a
olo <snash@tutanota.com>
2022-01-12 17:06:23 -0300

    OneProduct pg in progress

--- src/pages/OneProduct/OneProduct.jsx
+++ src/pages/OneProduct/OneProduct.jsx
@@ -2,14 +2,61 @@ import Nav from '../../components/Nav/Nav'
 import Announcements from '../../components/Announcements/Announcement'
 import Footer from '../../components/Footer/Footer'
 import NewsLetter from '../../components/NewsLetter/NewsLetter'
-import { Container } from './OneProduct.styles'
+import { Container,
+  Title, 
+  Image, 
+  ImgContainer, 
+  InfoContainer, 
+  Wrapper, 
+  Desc, 
+  Price,
+  FilterContainer,
+  FilterTitle,
+  Filter,
+  FilterColor,
+  FilterSize,
+  FilterSizeOption 
+} from './OneProduct.styles'
+
 
 const OneProduct = () => {
   return (
     <Container>
       <Nav />
       <Announcements />
-      
+      <Wrapper>
+        <ImgContainer>
+          <Image src='https://i.ibb.co/S6qMxwr/jean.jpg' />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione necessitatibus iusto,
+            quaerat aut alias corrupti saepe temporibus nostrum optio molestias vero dolorum veritatis 
+            possimus blanditiis vitae assumenda eum corporis. Ratione?
+            Corporis, perferendis quae. Incidunt repellendus ipsam numquam nesciunt temporibus dolorum
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color='black' />
+              <FilterColor color='darkblue' />
+              <FilterColor color='gray' />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+        </InfoContainer>
+      </Wrapper>
       <NewsLetter />
       <Footer />
     </Container>
--- src/pages/OneProduct/OneProduct.styles.js
+++ src/pages/OneProduct/OneProduct.styles.js
@@ -3,4 +3,63 @@ import styled from 'styled-components'
 export const Container = styled.div`
 
 `
+export const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`
+export const ImgContainer = styled.div`
+  flex: 1;
+`
+export const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`
+export const Title = styled.h1`
+  
+`
+export const Desc = styled.p`
+  margin: 20px 0;
+`
+export const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`
+export const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`
+export const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const FilterTitle = styled.div`
+  font-size: 20px;
+  margin: 10px;
+`
+export const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`
+export const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+  background: transparent;
+  border: none;
+  border-left: 3px solid antiquewhite;
+  border-bottom: 3px solid antiquewhite;
+  font-size: 16px;
+`
+export const FilterSizeOption = styled.option`
 
+`
