a72b95820db4c1e4bdfdba3a32e4820dbc713237
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-02-25 09:47:49 -0300

    Models in progress

---
+++ Api/Models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  desc: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  categories: {
+    type: Array,
+    required: true
+  },
+  size: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true }
+);
+
+mongoose.exports = mongoose.Model('Product', productSchema);
